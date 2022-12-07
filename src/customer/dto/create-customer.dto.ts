8795db3fdd52fc6833b19aa65718a1c8cdf2c4dc
Jardejane <janefeitosalima49@gmail.com>
2022-12-06 22:56:11 -0300

    feat:teste

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,10 @@
-export class CreateCustomerDto {}
+import {}
+
+export class CreateCustomerDto {
+    id :string;
+    name :string;
+    email: string;
+    cpf: string;
+    password : string;
+    address : string;
+}
