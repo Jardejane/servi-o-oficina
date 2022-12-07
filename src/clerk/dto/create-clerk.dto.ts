6380d43ba33280eacffb39719515cf5edce2dbc8
Jardejane <janefeitosalima49@gmail.com>
2022-12-07 16:19:52 -0300

    feat: creating the dto in clerck

--- src/clerk/dto/create-clerk.dto.ts
+++ src/clerk/dto/create-clerk.dto.ts
@@ -1,1 +1,17 @@
-export class CreateClerkDto {}
+import { IsEmail, IsString } from "class-validator";
+export class CreateClerkDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: String;
+
+  @IsString()
+  isAdmin: String;
+}
