37b1d13c2ade268b2d69a144b53adb15693a3f33
Jardejane <janefeitosalima49@gmail.com>
2022-12-07 13:35:02 -0300

    feat: create the DTo in customer

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,10 +1,21 @@
-import {}
+import { IsString, IsEmail} from "class-validator";
 
 export class CreateCustomerDto {
+    @IsString()
     id :string;
+
+    @IsString()
     name :string;
+
+    @IsEmail()
     email: string;
+
+    @IsString()
     cpf: string;
+
+    @IsString()
     password : string;
+
+    @IsString()
     address : string;
 }
