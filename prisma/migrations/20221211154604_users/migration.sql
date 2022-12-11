d38419783d9f0ff0e8d1d606ca6a1bc822d7b904
Jardejane <janefeitosalima49@gmail.com>
2022-12-11 13:07:59 -0300

    feat: swappig the sqlite bank to postgreSQL

---
+++ prisma/migrations/20221211154604_users/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Clerk" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" TEXT NOT NULL,
+    "customerId" TEXT,
+
+    CONSTRAINT "Clerk_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Services" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "customerId" TEXT,
+    "clerkId" TEXT,
+
+    CONSTRAINT "Services_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "Customer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_cpf_key" ON "Customer"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Clerk_email_key" ON "Clerk"("email");
+
+-- AddForeignKey
+ALTER TABLE "Clerk" ADD CONSTRAINT "Clerk_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Services" ADD CONSTRAINT "Services_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Services" ADD CONSTRAINT "Services_clerkId_fkey" FOREIGN KEY ("clerkId") REFERENCES "Clerk"("id") ON DELETE SET NULL ON UPDATE CASCADE;
