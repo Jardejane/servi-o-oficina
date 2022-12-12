7d335cd291c3d240b24aeba15a92869f612e83de
Jardejane <janefeitosalima49@gmail.com>
2022-12-12 13:44:39 -0300

    create the method delete in services

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,5 +1,5 @@
 import { dtoServiceCreate } from './dto/create-services';
-import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 import { ServicesService } from './services.service';
 
 @Controller('services')
@@ -21,5 +21,10 @@ export class ServicesController {
   @Patch(':id')
   async update(@Param('id')id: string, @Body()data: dtoServiceCreate){
     return await this.services.upadate(data, id)
+  }
+
+  @Delete(':id')
+  async delete(@Param('id')id : string){
+    return await this.services.remove(id)
   }
 }
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -3,29 +3,38 @@ import { dtoServiceCreate } from './dto/create-services';
 import { PrismaService } from 'prisma/prisma.service';
 @Injectable()
 export class ServicesService {
-constructor( private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-async create (data: dtoServiceCreate): Promise<dtoServiceCreate>{
-    return await this.prisma.services.create({data})
-}
-async  getAllService(): Promise<dtoServiceCreate[]>{
-    return await this.prisma.services.findMany()
-}
-async getIdService(id):Promise<dtoServiceCreate>{
-    return await this.prisma.services.findUnique({
-        where:{
-            id
-        }
-    })
-}
-async upadate(data: dtoServiceCreate, id: string){
-    return this.prisma.services.update({
-        data, 
-        where:{
-           id 
-        }
-    })
-}
+    async create(data: dtoServiceCreate): Promise<dtoServiceCreate> {
+        return await this.prisma.services.create({ data })
+    }
+    async getAllService(): Promise<dtoServiceCreate[]> {
+        return await this.prisma.services.findMany()
+    }
+    async getIdService(id): Promise<dtoServiceCreate> {
+        return await this.prisma.services.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+    async upadate(data: dtoServiceCreate, id: string) {
+        return await this.prisma.services.update({
+            data,
+            where: {
+                id
+            }
+        })
+    }
+
+    async remove(id: string){
+        return await this.prisma.services.delete({
+            where: {
+                id
+            }
+        })
+    }
+ 
 
 
 
