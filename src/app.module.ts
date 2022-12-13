6fc339d08bc65cf5dadb252f64cf52597dbfb23b
Jardejane <janefeitosalima49@gmail.com>
2022-12-12 23:17:58 -0300

    create the crud Order

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { ClerkModule } from './clerk/clerk.module';
 import { ServicesModule } from './services/services.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [CustomerModule, ClerkModule, ServicesModule],
+  imports: [CustomerModule, ClerkModule, ServicesModule, OrderModule],
   controllers: [],
   providers: [],
 })
