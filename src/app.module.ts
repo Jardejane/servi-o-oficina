4cd705d4ce89c59b6bfdd82bb8774f1525b59956
Jardejane <janefeitosalima49@gmail.com>
2022-12-13 18:02:03 -0300

    create the auth with dto and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CustomerModule } from './customer/customer.module';
 import { ClerkModule } from './clerk/clerk.module';
 import { ServicesModule } from './services/services.module';
 import { OrderModule } from './order/order.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CustomerModule, ClerkModule, ServicesModule, OrderModule],
+  imports: [AuthModule, CustomerModule, ClerkModule, ServicesModule, OrderModule],
   controllers: [],
   providers: [],
 })
---
+++ src/auth/dto/dto-response.ts
@@ -0,0 +1,11 @@
+import { CreateClerkDto } from "src/clerk/dto/create-clerk.dto";
+import { CreateCustomerDto } from "src/customer/dto/create-customer.dto";
+import { ApiProperty } from '@nestjs/swagger'
+
+export class loginCustomerResponseDto{
+    @ApiProperty()
+    token:string;
+
+    @ApiProperty()
+    customer:CreateCustomerDto 
+}
